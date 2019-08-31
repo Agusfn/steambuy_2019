847a5c18e3b6ad43bda2e7fb1eb2c40cc15363a2
Agustin FN <agusfn20@gmail.com>
2019-08-31 13:49:40 -0300

    .

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $steam_sales_featured_items = 9;
 		<script type="text/javascript" src="global_scripts/js/jquery-1.8.3.min.js"></script>
 		<script type="text/javascript" src="global_design/bootstrap-3.1.1/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="resources/js/global-scripts.js"></script>
-		<script type="text/javascript" src="resources/js/main-page.js"></script>
+		<script type="text/javascript" src="resources/js/main-page.js?2"></script>
 		<script type="text/javascript" src="resources/js/price-inpt-fnc.js"></script>
 
     </head>
